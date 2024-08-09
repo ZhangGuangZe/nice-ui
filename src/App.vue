1054b89f45e47829a7f22452a1a241c729278db8
ZhangGuangZe <zhanggz1997@126.com>
2024-08-09 16:47:47 +0800

    feat: 增加 tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,11 @@
 <script setup lang="ts">
-// import HelloWorld from './components/HelloWorld.vue'
-import Test from './components/Test.tsx'
+import HelloWorld from './components/HelloWorld.vue'
+// import Test from './components/Test.tsx'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
-  <Test />
+  <HelloWorld msg="Vite + Vue" />
+  <!-- <Test /> -->
 </template>
 
 <style scoped>
