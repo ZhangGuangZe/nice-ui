c18340eeb50bb5a6c2d647493fc123d5155e98eb
ZhangGuangZe <zhanggz1997@126.com>
2024-10-28 18:08:54 +0800

    build: 处理 sass 控制台报错问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-/// <reference types="vitest" />
+/// <reference types="vitest/config" />
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +12,19 @@ export default defineConfig({
     globals: true,
     environment: 'happy-dom',
     transformMode: {
-      web: [/[jt]sx$/]
+      web: [/\.[jt]sx$/]
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler' // or "modern", "legacy"
+      }
     }
   }
 })
